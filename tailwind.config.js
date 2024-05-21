5981cd5eb561f7d22b30016ccc36bc37f22a94c2
wanderlee <wanderleedev@gmail.com>
2024-05-20 22:25:40 -0500

    feat: add fonts and logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-import animations from '@midudev/tailwind-animations'
+import animations from "@midudev/tailwind-animations";
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Belwe: ["Belwe-bold"],
+      },
+    },
   },
   plugins: [animations],
-}
+};
